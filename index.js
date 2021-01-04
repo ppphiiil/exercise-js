bb33b8ec1602e8e81a09d2717a555c5911492f79
ppphiiil <info@philmeinwelt.com>
2021-01-04 13:34:56 +0100

    exercise done

---
+++ index.js
@@ -0,0 +1,9 @@
+const text = `this is an exercise in console logging`;
+
+console.log(text);
+console.log(`${text}`);
+
+const firstName = `philipp`;
+const lastName = `moser`;
+
+console.log(`${firstName} ${lastName}`);
